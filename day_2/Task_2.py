f3d941ff6e51190ba8872900a26b9bb8628d827a
rashi-ash <83757768+rashi-ash@users.noreply.github.com>
2021-06-08 17:27:40 +0530

    Create Task_2.py

---
+++ day_2/Task_2.py
@@ -0,0 +1,12 @@
+i=2
+print(float(i))
+
+#2 )Consider x="Hello" and y="World" , then write a program to concatinate the strings to a single string and print the result.
+x="Hello "
+y="World"
+print(x+y)
+
+#3 ) Consider pi = 3.14 . print the value of pie and its type.
+pi=3.14
+print(pi)
+type(pi)
